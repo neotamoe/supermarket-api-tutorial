e7a965492046de37eec01c07e214e2cabe4891fb
Neota Moe <neota.moe@gmail.com>
2019-09-13 18:46:22 -0500

    add repository, update service

--- src/Services/CategoryService.cs
+++ src/Services/CategoryService.cs
@@ -8,8 +8,16 @@ namespace Supermarket.API.Services
 {
     public class CategoryService : ICategoryService
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            this._categoryRepository = categoryRepository;
+        }
         public async Task<IEnumerable<Category>> ListAsync()
         {
+            return await _categoryRepository.ListAsync();
+
         }
     }
 }
