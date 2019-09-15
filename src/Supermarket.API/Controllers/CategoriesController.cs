f0063f2009a7354d9eab4e58f224de4f9e1f1c69
Neota Moe <neota.moe@gmail.com>
2019-09-14 19:11:38 -0500

    add new resource, validate request body using modelstate, map new resource

--- src/Supermarket.API/Controllers/CategoriesController.cs
+++ src/Supermarket.API/Controllers/CategoriesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Supermarket.API.Domain.Models;
 using Supermarket.API.Domain.Services;
 using Supermarket.API.Resources;
+using Supermarket.API.Extensions;
 
 namespace Supermarket.API.Controllers
 {
@@ -28,6 +29,16 @@ namespace Supermarket.API.Controllers
             var resources = _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryResource>>(categories);
 
             return resources;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] SaveCategoryResource resource)
+        {
+            	if (!ModelState.IsValid)
+                    return BadRequest(ModelState.GetErrorMessages());   
+
+                var category = _mapper.Map<SaveCategoryResource, Category>(resource);
+
         }
     }
 }
