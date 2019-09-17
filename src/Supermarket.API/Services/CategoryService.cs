00016397b88968d3b55cf6f3aa9788d804a36629
Neota Moe <neota.moe@gmail.com>
2019-09-16 20:25:23 -0500

    add update logic

--- src/Supermarket.API/Services/CategoryService.cs
+++ src/Supermarket.API/Services/CategoryService.cs
@@ -25,20 +25,43 @@ namespace Supermarket.API.Services
 
         }
 
-        	public async Task<SaveCategoryResponse> SaveAsync(Category category)
+        public async Task<SaveCategoryResponse> SaveAsync(Category category)
+        {
+            try
             {
-                try
-                {
-                    await _categoryRepository.AddAsync(category);
-                    await _unitOfWork.CompleteAsync();
-                    
-                    return new SaveCategoryResponse(category);
-                }
-                catch (Exception ex)
-                {
-                    // Do some logging stuff
-                    return new SaveCategoryResponse($"An error occurred when saving the category: {ex.Message}");
-                }
+                await _categoryRepository.AddAsync(category);
+                await _unitOfWork.CompleteAsync();
+                
+                return new SaveCategoryResponse(category);
             }
+            catch (Exception ex)
+            {
+                // Do some logging stuff
+                return new SaveCategoryResponse($"An error occurred when saving the category: {ex.Message}");
+            }
+        }
+
+        public async Task<SaveCategoryResponse> UpdateAsync(int id, Category category)
+        {
+            var existingCategory = await _categoryRepository.FindByIdAsync(id);
+
+            if (existingCategory == null)
+                return new SaveCategoryResponse("Category not found.");
+
+            existingCategory.Name = category.Name;
+
+            try
+            {
+                _categoryRepository.Update(existingCategory);
+                await _unitOfWork.CompleteAsync();
+
+                return new SaveCategoryResponse(existingCategory);
+            }
+            catch (Exception ex)
+            {
+                // Do some logging stuff
+                return new SaveCategoryResponse($"An error occurred when updating the category: {ex.Message}");
+            }
+        }
     }
 }
