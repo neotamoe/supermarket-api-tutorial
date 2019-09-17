11a257bc92b0ab30598249e4ca3ad34a2ab5bee4
Neota Moe <neota.moe@gmail.com>
2019-09-16 20:17:16 -0500

    update controller and service interface for PUT

--- src/Supermarket.API/Domain/Services/ICategoryService.cs
+++ src/Supermarket.API/Domain/Services/ICategoryService.cs
@@ -9,6 +9,7 @@ namespace Supermarket.API.Domain.Services
     {
          Task<IEnumerable<Category>> ListAsync();
         Task<SaveCategoryResponse> SaveAsync(Category category);
+        Task<SaveCategoryResponse> UpdateAsync(int id, Category category);
 
     }
 }
